d1b2a2a5d7d5a284fb482dbd52189d5f347cf17e
Oleg Sichev <sichev.oleg@bk.ru>
2023-05-23 15:34:04 +0300

    fix description of the variable

--- src/main/java/JvmComprehension.java
+++ src/main/java/JvmComprehension.java
@@ -1,7 +1,8 @@
 public class JvmComprehension {
 
     public static void main(String[] args) {
-        int i = 1;                      // 1 Поле прописывает в Metaspace т.к. тут int, а не Integer
+        int i = 1;                      // 1 Поле прописывается в Stack т.к. тут int, а не Integer (значение переменной
+        // примитивного типа)
         Object o = new Object();        // 2 В куче создает новый объект, в стэке прописывается ссылка на него
         Integer ii = 2;                 // 3 В куче создает целочисленное значение, в стэке прописывает ссылку на него
         printAll(o, i, ii);             // 4 Метод прописывается в Metaspace
